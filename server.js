1a8f0dced55a7bfe8513d21dc41f49b2cb3ed211
ShahEmaad <115357853+ShahEmaad@users.noreply.github.com>
2023-07-20 15:22:40 +0530

    Build

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ import colors from 'colors';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
 import connectDB from './config/db.js';
+
 import authRoutes from './routes/authRoute.js';
+import productRoutes from './routes/productRoute.js';
+import categoryRoutes from './routes/categoryRoute.js';
+
+import path from "path";
 
 //Concurrent Servers
 import cors from 'cors';
@@ -23,12 +28,16 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
+app.use(express.static(path.join(__dirname, './client/build')))
+
 //routes
 app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/product', productRoutes);
+app.use('/api/v1/category', categoryRoutes);
 
 //rest api
-app.get('/',(req,res) => {
-    res.send("<h1>Welcome</h1>");
+app.use('*',function(req,res){
+    res.sendFile(path.join(__dirname, './client/build/index.html'))
 });
 
 
